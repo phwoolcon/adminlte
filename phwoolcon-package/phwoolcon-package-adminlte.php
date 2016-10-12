d2312397b8c28d53865e573b0527134047f98be5
Christopher CHEN <chenshaoming@appgame.com>
2016-10-12 20:37:02 +0800

    feat: auto refresh code mirror

--- phwoolcon-package/phwoolcon-package-adminlte.php
+++ phwoolcon-package/phwoolcon-package-adminlte.php
@@ -14,6 +14,7 @@ return [
                 'codemirror-5.19.0/addon/edit/matchbrackets.js',
                 'codemirror-5.19.0/addon/comment/continuecomment.js',
                 'codemirror-5.19.0/addon/comment/comment.js',
+                'codemirror-5.19.0/addon/display/autorefresh.js',
                 'codemirror-5.19.0/mode/javascript/javascript.js',
             ],
         ],
