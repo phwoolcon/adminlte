18d0c14fee37856eb80e3c11ce54a3e63955cc4c
Christopher CHEN <chenshaoming@appgame.com>
2016-10-12 19:00:27 +0800

    feat: add code mirror for json

--- phwoolcon-package/phwoolcon-package-adminlte.php
+++ phwoolcon-package/phwoolcon-package-adminlte.php
@@ -1,5 +1,21 @@
 <?php
 return [
     'appgame/phwoolcon-AdminLTE' => [
+        'admin_assets' => [
+            'adminlte-css' => [
+            ],
+            'adminlte-js' => [
+            ],
+            'codemirror-css' => [
+                'codemirror-5.19.0/lib/codemirror.css',
+            ],
+            'codemirror-json-js' => [
+                'codemirror-5.19.0/lib/codemirror.js',
+                'codemirror-5.19.0/addon/edit/matchbrackets.js',
+                'codemirror-5.19.0/addon/comment/continuecomment.js',
+                'codemirror-5.19.0/addon/comment/comment.js',
+                'codemirror-5.19.0/mode/javascript/javascript.js',
+            ],
+        ],
     ],
 ];
